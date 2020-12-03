8dc874a8679df4b4fb09defc892fc12e8ea5eee0
dreamiAsh <ashlyn.leng@gmail.com>
2020-12-02 20:12:41 -0800

    try

--- src/Components/RecipeList/RecipeList.jsx
+++ src/Components/RecipeList/RecipeList.jsx
@@ -3,25 +3,26 @@ import styles from "./RecipeList.module.css";
 import RecipeItem from "../RecipeItem/RecipeItem"
 import { isEmpty } from "lodash";
 
-const RecipeList = () => {
+const RecipeList = (props) => {
 
-  const [fetchedData, setFetchedData] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-      // put data fetching code here!
-      // performs a GET request
-      const response = await fetch(
-        "http://demo9533681.mockable.io/recipeList"
-      );
-      const responseJson = await response.json();
-      setFetchedData(responseJson);
-    };
-    if(isEmpty(fetchedData)) {
-      fetchData();
-    }
-  }, [fetchedData]);
+  // const [fetchedData, setFetchedData] = useState({});
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     // put data fetching code here!
+  //     // performs a GET request
+  //     const response = await fetch(
+  //       "http://demo9533681.mockable.io/recipeList"
+  //     );
+  //     const responseJson = await response.json();
+  //     setFetchedData(responseJson);
+  //   };
+  //   if(isEmpty(fetchedData)) {
+  //     fetchData();
+  //   }
+  // }, [fetchedData]);
 
-    const myList = Object.values(fetchedData)
+    // const myList = Object.values(fetchedData)
+    const myList = props.recipes
     const mappedList = myList.map((listItem) => (
       <RecipeItem recipe={listItem}/>
     ))
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,7 +4,7 @@ import NewsList from "../Components/NewsList/NewsList"
 import RecipeItem from "../Components/RecipeItem/RecipeItem"
 import RecipeList from "../Components/RecipeList/RecipeList"
 
-const Home = () => {
+const Home = (props) => {
 
     return (
         <div>
@@ -12,7 +12,7 @@ const Home = () => {
                 <img className={styles.img} src="https://images.squarespace-cdn.com/content/v1/5dedf21b7c131a3015fd5ad5/1575948055123-VLZ49JEXIYF8QMAABP6F/ke17ZwdGBToddI8pDm48kGNd1IaaP_-ea7nYNEFHDsp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UUIsyChEB7CNmEcY1NH_HD7WSuQ7QDejvk14LXDmYznTAN43Xjxxvi5UJclDbUAqLw/DSC04905-43_web.jpg?format=2500w" alt="food poster"/>
             </div>
             <NewsList/>
-            <RecipeList/>
+            <RecipeList recipes={props.recipes}/>
         </div>
     )
 }
